2b535d666aa31daf7ff2751a412163e51a18f9b0
Amit <amitbansal.dtu@gmail.com>
2017-06-24 14:18:46 +0530

    more changes

--- app/src/main/java/com/android/firebasedemo/myrail/SplashScreenActivity.java
+++ app/src/main/java/com/android/firebasedemo/myrail/SplashScreenActivity.java
@@ -1,6 +1,8 @@
 package com.android.firebasedemo.myrail;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -16,14 +18,19 @@ public class SplashScreenActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+                openHomeScreen();
             }
-        });
+        }, 300);
+    }
+
+    private void openHomeScreen() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 
 }
