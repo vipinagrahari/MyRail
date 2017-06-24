7c339edd74d5139016bd2e35d9606d58fb12d792
Vipin Agrahari <vipinkagrahari@gmail.com>
2017-06-24 11:34:41 +0530

    Initial commit

---
+++ app/src/main/java/com/android/firebasedemo/myrail/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.android.firebasedemo.myrail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
